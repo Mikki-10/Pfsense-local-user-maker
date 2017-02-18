be2dfa2d1b3bdfd2d3c5724abe38e4e16be8c509
Mikki Sørensen <Mikki-10@hotmail.com>
2017-02-18 23:01:51 +0100

    giver funktunalitet

--- index.php
+++ index.php
@@ -1,28 +1,53 @@
 <!DOCTYPE html>
 <html> 
     <head>
-        <title>Pfsense Grafer</title>
+        <title>Pfsense User Maker</title>
         <link rel="shortcut icon" href="favicon.ico" />
         
 <?php
 
 ini_set("display_errors", true);
 
-include "config.php";
-include "funktioner.php";
+//include "config.php";
+include "functions.php";
 
 if ($_POST != NULL) 
 {
-    var_dump($_POST);
+    //var_dump($_POST);
 
-    $pfsense = new pfsense;
-    $pfsense->login($_POST["server"], $_POST["username"], $_POST["password"]);
-    if (isset($_POST["delete_users"])) 
+    $pfsense = new pfsense($_POST["server"], $_POST["http-https"]);
+    
+    $pfsense->login($_POST["username"], $_POST["password"]);
+
+    if (isset($_POST["only-delete-users"])) 
     {
-        //Slet brugere
+        $pfsense->delete_users($_POST["prefix"]);
+
+        $gui = new gui;
+        $gui->show_form(TRUE);
     }
-    $pfsense->make_user($_POST["server"], $_POST["prefix"], isset($_POST["password_as_comment"]));
-	//Vis resultat
+    elseif (isset($_POST["submit"]))
+    {
+        if (isset($_POST["delete-users"])) 
+        {
+            $pfsense->delete_users($_POST["prefix"]);
+        }
+
+        $users = $pfsense->make_user($_POST["prefix"], $_POST["user-amount"], isset($_POST["password-as-comment"]));
+        //Vis resultat
+        //echo "<pre>"; var_dump($users); echo "</pre>";
+        
+        foreach ($users as $key => $user) 
+        {
+            echo "Username: ";
+            echo $user["username"];
+            echo "<br>";
+            echo "Password: ";
+            echo $user["password"];
+            echo "<br>";
+            echo "<br>";
+        }
+    }
 }
 else
 {
