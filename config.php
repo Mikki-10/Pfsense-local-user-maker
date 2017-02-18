7732bc0d6496c98ec20e8654186a84195bbd100f
Mikki Sørensen <Mikki-10@hotmail.com>
2017-02-18 23:00:25 +0100

    dropper config.php

--- config.php
+++ config.php
@@ -2,7 +2,9 @@
 
 //set the directory for the cookie using defined document root var
 //for pfsense curl
-$cookie_file_path = "C://temp/cookie-user-maker.txt";
+//$cookie_file_path = "C://temp/cookie-user-maker.txt";
+
+//$user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7";
 
 
 
