a776a3b7cd49305dedc28714342b00172ee27aaa
Mikki Sørensen <Mikki-10@hotmail.com>
2017-02-19 06:23:07 +0100

    oprydning af filer ældre end 24 timer i uploads mappen

--- index.php
+++ index.php
@@ -60,6 +60,19 @@ else
     //$gui->print("print", array(array("username" => "tgvlan1", "password" => "1234"), array("username" => "tgvlan2", "password" => "2345"), array("username" => "tgvlan3", "password" => "3456")) );
 }
 
+// Clean up old files uploaded
+/** define the directory **/
+$dir = __DIR__ . "/uploads/";
+
+/*** cycle through all files in the directory ***/
+foreach (glob($dir."*") as $file) {
+
+/*** if file is 24 hours (86400 seconds) old then delete it ***/
+if (filemtime($file) < time() - 86400) {
+    unlink($file);
+    }
+}
+
 
 
 // --------------------------------------------------------- //
